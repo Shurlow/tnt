386df58ada063dcc0ed8444106dd3708082f4f6e
Scott Hurlow <Scottyhurlow@gmail.com>
2018-11-16 14:29:32 -0800

    porting files over from project

---
+++ content.js
@@ -0,0 +1,12 @@
+console.log('Selecting:');
+
+$('#drawbridge-root').remove()
+$('.wp_signin').remove()
+$('html.drawbridge-up').css('overflow', 'visible')
+$('html.drawbridge-up #pb-root').css('overflow', 'visible')
+$('html.drawbridge-up body').css('overflow', 'visible')
+// $('.pb-theme-normal').css('overflow-y', 'visible')
+// $('.pb-theme-normal').css('overflow', 'visible')
+// $('body').css('overflow-y', 'visible')
+// $('body').css('overflow', 'visible')
+// $('html').css('overflow', 'visible')
---
+++ index.js
@@ -0,0 +1,39 @@
+// console.log('Hello ram', $('#drawbridge-root').remove());
+
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   // No tabs or host permissions needed!
+//   // console.log('Turning ' + tab.url + ' red!');
+//   // chrome.tabs.executeScript({
+//   //   code: 'document.body.style.backgroundColor="red"'
+//   // });
+//   console.log(document);
+//
+//
+//   chrome.tabs.executeScript(1, {
+//     file: 'content.js'
+//   })
+//
+// });
+// console = chrome.extension.getBackgroundPage().console;
+// console.assert('WAAA')
+console.log('Hello', chrome);
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.executeScript({ file: "jquery-3.1.1.min.js" }, function() {
+      chrome.tabs.executeScript({ file: "content.js" });
+  });
+});
+
+
+
+// var tab_id = -1;
+//
+// chrome.tabs.getSelected(null, function(tab) {
+//   tab_id = tab.id;
+// });
+//
+// chrome.extension.onRequest.addListener(function(request, sender) {
+//   console.log("In background listener", request);
+//   chrome.tabs.executeScript(tab_id, {
+//     file:"content.js"
+//   });
+// });
